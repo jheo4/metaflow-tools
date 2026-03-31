91cf27e52c04fdd0e2a1f18aaa8e4650a454a182
Jin Heo <993jin@gmail.com>
2026-03-30 22:20:19 -0400

    fix env var

--- charts/metaflow/helm_install.sh
+++ charts/metaflow/helm_install.sh
@@ -1,4 +1,5 @@
 helm dep build
 cat aws_eks_values.yaml | envsubst
+rm aws_secret.yaml
 cat aws_secret_example.yaml | envsubst >>aws_secret.yaml
-cat aws_eks_values.yaml | envsubst | helm upgrade --install metaflow . -n $AWS_EKS_NAMESPACE -f - -f aws_secret.yaml
+cat aws_eks_values.yaml | envsubst | helm upgrade --install metaflow . -n $AWS_CLUSTER_NAMESPACE -f - -f aws_secret.yaml
